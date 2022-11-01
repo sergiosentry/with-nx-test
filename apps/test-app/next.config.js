3f1a8d9b6cfbcf05f25a7895b8cc1a3a923a3ecf
Sergio Lombana <sergiolombana@YW0RH3K6N0-Sergio-Lombana.local>
2022-11-01 18:35:23 -0400

    removing unnecessary config

--- apps/test-app/next.config.js
+++ apps/test-app/next.config.js
@@ -6,101 +6,12 @@ const { withNx } = require('@nrwl/next/plugins/with-nx');
 const { withSentryConfig } = require('@sentry/nextjs');
 
 const { ModuleFederationPlugin } = require('webpack').container;
-const path = require('path');
-
-const contentSecurityPolicy = `
-  frame-ancestors 'self' *.google.com *.googleusercontent.com
-`;
-
-const securityHeaders = [
-  {
-    key: 'Content-Security-Policy',
-    value: contentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
-  },
-  {
-    key: 'Referrer-Policy',
-    value: 'strict-origin-when-cross-origin'
-  },
-  {
-    key: 'Strict-Transport-Security',
-    value: 'max-age=15768000; includeSubDomains; preload'
-  },
-  {
-    key: 'X-Content-Type-Options',
-    value: 'nosniff'
-  },
-  {
-    key: 'X-Frame-Options',
-    value: 'DENY'
-  },
-  {
-    key: 'X-XSS-Protection',
-    value: '1; mode=block'
-  }
-];
-
-const otherHeaders = [
-  {
-    key: 'Edge-Cache-Tag',
-    value: 'gemini, gemini_origins-com'
-  }
-];
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
-  experimental: {
-    optimizeCss: true,
-    newNextLinkBehavior: true
-  },
-  headers: async () => {
-    return [
-      {
-        source: '/:path*',
-        headers: [...securityHeaders, ...otherHeaders]
-      }
-    ];
-  },
-  images: {
-    loader: 'akamai',
-    path: 'https://www.origins.com/'
-  },
-  nx: {
-    svgr: true
-  },
-  poweredByHeader: false,
-  productionBrowserSourceMaps: true,
-  rewrites: async () => {
-    return {
-      beforeFiles: [
-        {
-          source: '/',
-          destination: '/home'
-        },
-        {
-          source: '/media/:slug*',
-          destination: 'https://www.origins.com/media/:slug*'
-        }
-      ],
-      afterFiles: [],
-      fallback: []
-    };
-  },
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')]
-  },
   webpack: (config, { dev, isServer, ...options }) => {
-    if (process.env.ANALYZE) {
-      const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-      config.plugins.push(
-        new BundleAnalyzerPlugin({
-          analyzerMode: 'static',
-          reportFilename: './analyze/client.html'
-        })
-      );
-    }
-
 
     config.plugins.push(
       new ModuleFederationPlugin({
@@ -111,20 +22,10 @@ const nextConfig = {
       })
     );
 
-    const performance = dev
-      ? config.performance
-      : {
-          hints: 'error',
-          // ±300kb gzipped
-          maxAssetSize: 12000000, //TODO: rollback this to 1200000 when Content Mocks are removed.
-          maxEntrypointSize: 12000000 //TODO: rollback this to 1200000 when Content Mocks are removed.
-        };
-    config.performance = performance;
     return config;
   },
   sentry: {
     hideSourceMaps: false,
-    autoInstrumentServerFunctions: false,
   }
 };
 
